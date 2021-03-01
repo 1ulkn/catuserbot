96f708616ffaafd032d570e0ea54e50365f23c00
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-03-01 19:24:09 +0530

    Update catub_config.py

--- catuserbot/Config/catub_config.py
+++ catuserbot/Config/catub_config.py
@@ -5,7 +5,7 @@ import os
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
-    from sample_config import Config  # noqa
+    from Config import Config  # noqa
 else:
     if os.path.exists("config.py"):
         from config import Development as Config  # noqa
